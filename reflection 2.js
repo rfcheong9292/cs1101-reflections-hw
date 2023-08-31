364991ebdf607c94d9be8cbeab0b0a209d15e822
Source Academy User <No public email provided>
2023-08-31 10:46:37 +0800

    Changes made from Source Academy

--- reflection 2.js
+++ reflection 2.js
@@ -1,8 +1,3 @@
-function sum(term, a, next, b) {
-    
-    return iter(term, a, next, b, 0);
-}
-
 function iter(term, a, next, b, total) {
     return (a > b) ? total
                    : iter(term, next(a), next, b, total + term(a));
@@ -10,7 +5,7 @@ function iter(term, a, next, b, total) {
 }
 
 function my_sum(n) { 
-    return sum(x => x*(x+1), 1, x => x + 1, n); 
+    return iter(x => x*(x+1), 1, x => x + 1, n, 0); 
 }
 
 
